271215653feda7cbf82ae330795c27c19a0b56c4
AlexxSich <alexxsich@gmail.com>
2024-03-25 18:49:55 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -25,39 +25,37 @@ function userInputHandler(event) {
     const searchedImg = event.target.elements.search.value.trim();
     
     if(searchedImg !== "") {
-       getImages(searchedImg).then(images => {
-        if(images.hits.length === 0) {
-            iziToast.error({
-                position: 'topRight',
-                title: 'Error',
-                message: '❌ Sorry, there are no images matching your search query. Please try again!',
-              });
-              return;
-        }
-            const markup = imagesArr(images);
-            myGallery.innerHTML = markup;  
-            lightBox.refresh();
-            userInput.reset();
-
-// event.currentTarget.reset();
-// ??? не зрозумів чому .catch() ловить event.currentTarget.reset() як помилку
-                        
-        }).catch(error => {
-        iziToast.error({
-            position: 'topRight',
-            title: 'Error',
-            message: '❌ Something went wrong. Try again later.',
-          });
-        }).finally(() => {
-            loader.classList.remove("active");
-        });
-    } else {
-        iziToast.error({
-            position: 'topRight',
-            title: 'Error',
-            message: '❌ Please input your request in the search field',
-          });
-    };  
+       getImages(searchedImg)
+            .then(images => {
+                if(images.hits.length === 0) {
+                    iziToast.error({
+                        position: 'topRight',
+                        title: 'Error',
+                        message: '❌ Sorry, there are no images matching your search query. Please try again!',
+                    });
+                    return;
+                }
+                    const markup = imagesArr(images);
+                    myGallery.innerHTML = markup;  
+                    lightBox.refresh();
+                    userInput.reset();                                
+        })
+            .catch(error => {
+                iziToast.error({
+                    position: 'topRight',
+                    title: 'Error',
+                    message: '❌ Something went wrong. Try again later.',
+                });
+                }).finally(() => {
+                    loader.classList.remove("active");
+                });
+            } else {
+                iziToast.error({
+                    position: 'topRight',
+                    title: 'Error',
+                    message: '❌ Please input your request in the search field',
+                });
+        };  
     };   
 
     function imagesArr(arr) {
